1cac5392c3f135ff307602a0411bbf0fa50f0226
lucas <d2194499406@gmail.com>
2017-07-11 21:09:34 +0800

    simple-form select

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,23 +1,24 @@
 class Event < ApplicationRecord
 
- validates_presence_of :name, :friendly_id
 
- validates_uniqueness_of :friendly_id
- validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+  STATUS = ["draft", "public", "private"]
+  validates_inclusion_of :status, :in => STATUS
 
- before_validation :generate_friendly_id, :on => :create
+  validates_presence_of :name, :friendly_id
 
- def to_param
+  validates_uniqueness_of :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+
+  before_validation :generate_friendly_id, :on => :create
+
+  def to_param
    self.friendly_id
- end
+  end
 
- protected
+  protected
 
- def generate_friendly_id
+  def generate_friendly_id
    self.friendly_id ||= SecureRandom.uuid
- end
-
- STATUS = ["draft", "public", "private"]
- validates_inclusion_of :status, :in => STATUS
+  end
 
 end
