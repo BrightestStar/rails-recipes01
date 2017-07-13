aeb8f1bcbc194d4fe1ae00c6ea4d16b43f4198d3
lucas <d2194499406@gmail.com>
2017-07-13 10:46:32 +0800

    user jqurey-ui-rails gem reorder

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -70,7 +70,10 @@ class Admin::EventsController < AdminController
     @event.row_order_position = params[:position]
     @event.save!
 
-    redirect_to admin_events_path
+    redirect_to do |format|
+      format.html { redirect_to admin_events_path }
+      format.json { render :json => { :message => "ok" }}
+    end
   end
 
   protected
